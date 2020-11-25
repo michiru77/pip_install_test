36155b4ddf823d11808bf275aadcf1da2d056f11
michiru77 <k198579@ie.u-ryukyu.ac.jp>
2020-11-25 12:51:44 +0900

    print.py

---
+++ module2/module1.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+class Print():
+    def __init__(self) -> None:
+        self.date = datetime.today()
+        self.year = self.date.year
+        self.month = self.date.month
+        self.day = self.date.day
+
+    def do(self) -> None:
+        print('Hoge2 {} {} {}'.format(self.year, self.month, self.day))
