f4696da364f0b4c414f157db99d0860662cba45d
michiru77 <k198579@ie.u-ryukyu.ac.jp>
2020-11-25 14:10:56 +0900

    no message

--- hirosi/hira2kata.py
+++ hirosi/hira2kata.py
@@ -1,11 +1,14 @@
 from datetime import datetime
 
-class Print():
-    def __init__(self) -> None:
-        self.date = datetime.today()
-        self.year = self.date.year
-        self.month = self.date.month
-        self.day = self.date.day
+# class Print():
+#     def __init__(self) -> None:
+#         self.date = datetime.today()
+#         self.year = self.date.year
+#         self.month = self.date.month
+#         self.day = self.date.day
 
-    def do(self) -> None:
-        print('{} {} {}'.format(self.year, self.month, self.day))
+#     def do(self) -> None:
+#         print('{} {} {}'.format(self.year, self.month, self.day))
+
+def ddd():
+    print(datetime.today())
--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ import setuptools
 with open("hirosi/README.md", "r") as fh:
     hirosi_long_description = fh.read()
 
-with open("japatypo/README.md", "r") as fh:
-    japatypo_long_description = fh.read()
-
 setuptools.setup(
     name="hirosi",
     version="1.0",
@@ -13,23 +10,6 @@ setuptools.setup(
     author_email="XXX",
     description="module1 is my own python package",
     long_description=hirosi_long_description,
-    long_description_content_type="text/markdown",
-    url="XXX",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: XXX",
-        "License :: OSI Approved :: XXX",
-        "Operating System :: XXX",
-    ]
-)
-
-setuptools.setup(
-    name="japatypo",
-    version="1.0",
-    author="michiru77",
-    author_email="XXX",
-    description="module2 is my own python package",
-    long_description=japatypo_long_description,
     long_description_content_type="text/markdown",
     url="XXX",
     packages=setuptools.find_packages(),
