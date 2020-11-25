15b7aeaf3a87d7e922af5794e8acb5a1fd05676e
michiru77 <k198579@ie.u-ryukyu.ac.jp>
2020-11-25 14:22:59 +0900

    no message

--- setup.py
+++ setup.py
@@ -12,10 +12,30 @@ setuptools.setup(
     long_description=hirosi_long_description,
     long_description_content_type="text/markdown",
     url="XXX",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages('hirosi'),
     classifiers=[
         "Programming Language :: Python :: XXX",
         "License :: OSI Approved :: XXX",
         "Operating System :: XXX",
     ]
 )
+
+# with open("japatypo/README.md", "r") as fh:
+#     japatypo_long_description = fh.read()
+
+# setuptools.setup(
+#     name="japatypo",
+#     version="1.0",
+#     author="michiru77",
+#     author_email="XXX",
+#     description="module2 is my own python package",
+#     long_description=japatypo_long_description,
+#     long_description_content_type="text/markdown",
+#     url="XXX",
+#     packages=setuptools.find_packages(),
+#     classifiers=[
+#         "Programming Language :: Python :: XXX",
+#         "License :: OSI Approved :: XXX",
+#         "Operating System :: XXX",
+#     ]
+# )
